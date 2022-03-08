46f20744c8bca5a5ee9c4ff3e67673ecd0186446
ArindamSharma <arindamsharma1998@gmail.com>
2022-03-08 10:14:40 +0530

    milestone 1A

---
+++ constant.py
@@ -0,0 +1,19 @@
+# Keys
+TYPE="Type"
+EXECUTION="Execution"
+ACTIVITIES="Activities"
+INPUTS="Inputs"
+FUNCTION="Function"
+FUNCTIONINPUT="FunctionInput"
+EXECUTIONTIME="ExecutionTime"
+
+# Values
+SEQUENTIAL="Sequential"
+CONCURRENT="Concurrent"
+FLOW="Flow"
+TASK="Task"
+
+# Status Messages
+ENTRY="Entry"
+EXIT="Exit"
+EXECUTE="Execute"
--- main.py
+++ main.py
@@ -1,10 +1,57 @@
+from time import sleep
 import yaml
 import pandas as pd
+from datetime import datetime as dt
+import constant as C
+import threading
 
-def milestone1():
-    with open("Milestone1/Milestone1A.yaml","r") as f:
-        data=yaml.safe_load(f)
-        print(data)
+
+def log(*arg,**kwarg):
+    print(*arg,**kwarg,sep="")
+    # print(*arg,**kwarg)
+
+def taskHandler(taskName,data):
+    """Task handler Executes the Task"""
+    # Error handling
+    if(data[C.TYPE]!=C.TASK):
+        raise ValueError("FlowType required 'Flow', given ",data[C.TYPE])
+
+    log(dt.now(),";",taskName," Entry")
+    log(dt.now(),";",taskName," Execution ",data[C.FUNCTION],tuple(data[C.INPUTS].values()))
+    exeTime=int(data[C.INPUTS][C.EXECUTIONTIME])
+    sleep(exeTime)
+    log(dt.now(),";",taskName," Exit")
+
+def flowHandler(flowName,data):
+    """Flow handler Controls the Flow"""
+    # Error handling
+    if(data[C.TYPE]!=C.FLOW):
+        raise ValueError("FlowType required 'Flow', given ",data[C.TYPE])
+    
+    log(dt.now(),";",flowName," Entry")
+    if(data[C.EXECUTION]==C.SEQUENTIAL):
+        for activity in data[C.ACTIVITIES]:
+            if(data[C.ACTIVITIES][activity][C.TYPE]==C.TASK):
+                taskHandler(flowName+"."+activity,data[C.ACTIVITIES][activity])
+            elif(data[C.ACTIVITIES][activity][C.TYPE]==C.FLOW):
+                flowHandler(flowName+"."+activity,data[C.ACTIVITIES][activity])
+            else:
+                raise ValueError("Unknown Activity Type passed ",data[C.ACTIVITIES][activity][C.TYPE])
+
+    elif(data[C.EXECUTION]==C.CONCURRENT):
+        taskHandler(flowName+"."+activity,data[C.ACTIVITIES][activity])
+    else:
+        raise ValueError("Unknown Execution Parameter passed",data[C.EXECUTION])
+    
+    log(dt.now(),";",flowName," Exit")
+
+def milestone(path):
+    with open(path,"r") as f:
+        flowData=yaml.safe_load(f)
+        for flowName in flowData:
+            flowHandler(flowName,flowData[flowName])
 
 if __name__=="__main__":
-    milestone1()
+    clock=0
+    milestone("Milestone1/Milestone1A.yaml")
+    # milestone("Milestone1/Milestone1B.yaml")
